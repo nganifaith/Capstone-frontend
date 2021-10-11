c064e6a2a4ef35b282f98e52df24ddc9ff2004dd
ngani faith <nganifaith@gmail.com>
2021-10-11 20:58:16 +0100

    Add signup page

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import BeachDetails from './pages/BeachDetails';
 import Home from './pages/Home';
+import SignUpPage from './pages/SignUp';
 
 const Routes = () => (
   <BrowserRouter>
     <Header />
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/signup" component={SignUpPage} />
       <Route path="/beach/:id" component={BeachDetails} />
     </Switch>
   </BrowserRouter>

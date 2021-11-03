4f71d8b2652deea55971497dd778bec732d0c9c5
ngani faith <nganifaith@gmail.com>
2021-11-03 08:02:12 +0100

    update API request call

--- src/Api/auth.js
+++ src/Api/auth.js
@@ -2,16 +2,16 @@ import { getToken } from '../actions/currentUser';
 import sendRequest from './client';
 
 export function signIn(email, password) {
-  return sendRequest('/signin', 'POST', { auth: { email, password } });
+  return sendRequest('/user_token', 'POST', { auth: { email, password } });
 }
 
 export function getCurrentUser() {
   if (!getToken()) return Promise.reject();
-  return sendRequest('/me');
+  return sendRequest('/user');
 }
 
 export function signUp(name, email, password, confirmation) {
-  return sendRequest('/signup', 'POST', {
+  return sendRequest('/user', 'POST', {
     name, email, password, password_confirmation: confirmation,
   });
 }
